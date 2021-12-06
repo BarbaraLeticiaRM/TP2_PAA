a3dc8444cbcbfc997b09642f50af0fde1c7c99e8
Carlos Freitas <carlos.gabriel@aluno.ufop.edu.br>
2021-12-06 18:58:10 -0300

    argparsing

--- main.c
+++ main.c
@@ -1,17 +1,33 @@
 #include "algoritmos.h"
+#include <string.h>
 
-int main(){
+int main(int argc, char* argv[]){
     srand(time(NULL)); // seed aleatória
 
     Analise a;
-    int n;
+    int n, flagClique = 0, flagSatisfabilidade = 0;
     int* solucao;
     int** matriz;
     double media;
-    double tempos[3]; // Vetor para armazenar o tempo 
+    double tempos[3]; // Vetor para armazenar o tempo
 
-    // Conjunto Independente
-    printf("> Problema 1: Conjunto Independente\n");
+    // Processando qual problema deve ser executado
+    if(!strcmp(argv[1], "1") || !strcmp(argv[1], "ci")){
+        printf("> Problema 1: Conjunto Independente\n");
+    }
+    else if(!strcmp(argv[1], "2") || !strcmp(argv[1], "clique")){
+        printf("> Problema 2: Clique\n");
+        flagClique = 1;
+    }
+    else if(!strcmp(argv[1], "3") || !strcmp(argv[1], "satisfabilidade")){
+        printf("> Problema 3: Satisfabilidade\n");
+        flagSatisfabilidade = 1;
+    }
+    else{
+        printf("ERRO: O problema nao foi selecionado corretamente!\n");
+        return 0;
+    }
+
     for(int instancia = 0; instancia < 3; instancia++){
         iniciarTempo(&a);
         
@@ -22,42 +38,10 @@ int main(){
         else
             matriz = leGrafo("grafo3.txt", &n);
 
-        solucao = bbConjuntoIndependente(matriz, n);
-        printf("solucao:\n");
-        imprimeVetor(solucao, n);
-        printf("\n");
+        // Tratando o problema do Clique
+        if(flagClique)
+            entradaClique(matriz, n);
 
-        finalizarTempo(&a);
-        tempos[instancia] = (getDeltaTempo(&a));
-
-        // Desalocando a memória alocada dinamicamente
-        free(solucao);
-        for(int i = 0; i < n; i++)
-            free(matriz[i]);
-        free(matriz);
-    }
-
-    // Imprimindo o tempo gasto das instâncias e a média do tempo
-    media = calculaMedia(tempos, 3);
-
-    for(int instancia = 0; instancia < 3; instancia++){
-        printf("Tempo gasto na instancia %d: %.4lf segundos\n", instancia+1, tempos[instancia]);
-    }
-    printf("Media do tempo: %.4lf segundos\n", media);
-
-    // Clique
-    printf("> Problema 2: Clique\n");
-    for(int instancia = 0; instancia < 3; instancia++){
-        iniciarTempo(&a);
-        
-        if(instancia == 0)
-            matriz = leGrafo("grafo1.txt", &n);
-        else if(instancia == 1)
-            matriz = leGrafo("grafo2.txt", &n);
-        else
-            matriz = leGrafo("grafo3.txt", &n);
-
-        entradaClique(matriz, n);
         solucao = bbConjuntoIndependente(matriz, n);
         printf("solucao:\n");
         imprimeVetor(solucao, n);
